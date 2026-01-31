994f4cdbbdefe7536724e19a5e7dc709dcc5fcfa
TaTiaNaM <maluchenko.tania@gmail.com>
2026-01-31 22:10:37 +0200

    cat

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,37 +1,27 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import axios from "axios";
-import getImagesByQuery from './pixabay-api.js';
-const BASE_URL = "https://pixabay.com/api/";
-const imageContainer = document.querySelector('.gallery');
-getImagesByQuery();
-const options = { 
-     tags: 'alt',
-    largeImageURL: 'data-source',
-    webformatURL: 'src',
-    likes: 0,
-    views: 0,
-    comments: 0,
-    downloads: 0
-}
-const lightbox = new SimpleLightbox('.gallery_item a', options);
 
-export default function createGallery(images = [], options = {}) {
-    const imagesMarkup = images.map(({id, pageURL}) => {
+const imageContainer = document.querySelector('.gallery');
+
+
+const lightbox = new SimpleLightbox('.gallery_item a');
+
+export default function createGallery(images = []) {
+    const imagesMarkup = images.map(({id, pageURL, webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
         return `<li class="gallery_item" id="${id}">
-    <a href="${pageURL}"><img src="${options.webformatURL}"  data-source="${options.largeImageURL} alt="${options.tags}" loading="lazy" class="gallery_image"/>
+    <a href="${pageURL}"><img src="${webformatURL}"  data-source="${largeImageURL} alt="${tags}" loading="lazy" class="gallery_image"/>
     <div class="info">
         <p class="info_item">
-            <b>Likes</b> ${options.likes}
+            <b>Likes</b> ${likes}
         </p>
         <p class="info_item">   
-            <b>Views</b> ${options.views}
+            <b>Views</b> ${views}
         </p>
         <p class="info_item">
-            <b>Comments</b> ${options.comments}
+            <b>Comments</b> ${comments}
         </p>
         <p class="info_item">
-            <b>Downloads</b> ${options.downloads}
+            <b>Downloads</b> ${downloads}
         </p>
     </div>
     </a>
